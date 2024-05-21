869b4d5dc69b3089fd6ac85115bd77a9cdc5792e
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-21 15:42:25 +0200

    refactor: add second paddle

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ screen.bgcolor("black")
 screen.title("My Pong Game")
 screen.tracer(0)
 
-paddle = Paddle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkeypress(paddle.move_up, "Up")
-screen.onkeypress(paddle.move_down, "Down")
+screen.onkeypress(r_paddle.move_up, "Up")
+screen.onkeypress(r_paddle.move_down, "Down")
+screen.onkeypress(l_paddle.move_up, "w")
+screen.onkeypress(l_paddle.move_down, "s")
 
 game_is_on = True
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -2,13 +2,13 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.paddle = Turtle("square")
         self.paddle.color("white")
         self.paddle.penup()
         self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.paddle.goto(350, 0)
+        self.paddle.goto(position)
 
     def move_up(self):
         new_y = self.paddle.ycor() + 20
