5e24d2326758bc7b9f698cef6b5c9445467529e0
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-21 15:44:23 +0200

    refactor: cleanup constructor

--- paddle.py
+++ paddle.py
@@ -4,16 +4,16 @@ from turtle import Turtle
 class Paddle(Turtle):
     def __init__(self, position):
         super().__init__()
-        self.paddle = Turtle("square")
-        self.paddle.color("white")
-        self.paddle.penup()
-        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.paddle.goto(position)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
 
     def move_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def move_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
