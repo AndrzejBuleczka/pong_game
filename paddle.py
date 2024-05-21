345e2ebf614399328ac6e1b6d46eacf5b01c7738
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-21 15:37:22 +0200

    refactor: change go up and down methods

--- paddle.py
+++ paddle.py
@@ -1,17 +1,19 @@
 from turtle import Turtle
 
 
-class Paddle():
+class Paddle(Turtle):
     def __init__(self):
+        super().__init__()
         self.paddle = Turtle("square")
         self.paddle.color("white")
         self.paddle.penup()
-        self.paddle.shapesize(stretch_wid=1, stretch_len=5)
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
         self.paddle.goto(350, 0)
-        self.paddle.setheading(90)
 
     def move_up(self):
-        self.paddle.forward(20)
+        new_y = self.paddle.ycor() + 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
 
     def move_down(self):
-        self.paddle.backward(20)
+        new_y = self.paddle.ycor() - 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
